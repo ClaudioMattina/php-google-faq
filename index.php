f6c7f2bfb683391b5b429f65370e412fafc8a652
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-04-21 16:25:30 +0200

    preparazione array

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+/* Riscrivere questa pagina del sito google https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. */
+
+$faqs = [
+    [
+        'question' => '',
+        'answers' => ''
+    ],
+    [
+        'question' => '',
+        'answers' => ''
+    ],
+    [
+        'question' => '',
+        'answers' => ''
+    ],
+    [
+        'question' => '',
+        'answers' => ''
+    ],
+    [
+        'question' => '',
+        'answers' => ''
+    ],
+    [
+        'question' => '',
+        'answers' => ''
+    ],
+    
+];
+?>
